348f4e48002fbf8ca1e2aa5d9a9b982d2324eb86
Ana Saric <saric.ana456@gmail.com>
2023-10-25 13:52:10 +0200

    enteredBody variable  for output

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,10 @@
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  let enteredBody = "";
+
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    enteredBody = event.target.value;
   }
   return (
     <form className={classes.form}>
@@ -10,6 +12,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
