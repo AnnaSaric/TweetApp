47f286e32eb72f55c7590ffe6893514e85ece478
Ana Saric <saric.ana456@gmail.com>
2023-11-01 14:09:03 +0100

    Added Modal comoponent

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,21 +1,15 @@
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  let enteredBody = "";
-
-  function changeBodyHandler(event) {
-    enteredBody = event.target.value;
-  }
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,31 @@
+import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostsList() {
+  const [enteredBody, setEneredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEneredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
-        <Post author="Anuška" body="Moj prvi razumljivi prop!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Nada" body="React is awesome!!!" />
       </ul>
     </>
