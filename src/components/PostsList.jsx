deb0c450652779bcb69866b07c03df66a2fb993d
Ana Saric <saric.ana456@gmail.com>
2023-11-02 14:56:03 +0100

    Changed code for NewPost

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,31 +5,15 @@ import classes from "./PostsList.module.css";
 import { useState } from "react";
 
 function PostsList(props) {
-  const [enteredBody, setEneredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEneredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {props.isPosting ? (
         <Modal onClose={props.onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={props.onStopPosting}
-          />
+          <NewPost onCancel={props.onStopPosting} />
         </Modal>
       ) : null}
 
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Nada" body="React is awesome!!!" />
       </ul>
     </>
